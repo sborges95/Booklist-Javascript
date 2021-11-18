de05074e677962c2b1f208a350bad736dc20b566
sborges95 <shannon.borges@gmail.com>
2021-11-18 07:54:58 -0800

    initialization

---
+++ app.js
@@ -0,0 +1,54 @@
+// Book Class: Represents a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// UI Class: Handles UI Tasks
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: "Book One",
+        author: "John Doe",
+        isbn: "3434434"
+      },
+      {
+        title: "Book Two",
+        author: "Jane Doe",
+        isbn: "45545"
+      }
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td <a href="#" class ="btn btn-danger delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+}
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a Book
+
+// Event: Remove a Book
